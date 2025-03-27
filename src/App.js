8b1b202ec61eec2a32a279f68bce9dc6e8d26b01
Kal <134562542+TheRealObamacare@users.noreply.github.com>
2025-03-27 05:52:06 -0500

    why isnt it deploying correctly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { HashRouter as Router } from 'react-router-dom';
 import './css/App.css';
 import AppRouter from './AppRouter';
 
 function App() {
-  // Using HashRouter for better GitHub Pages compatibility
   return (
-    <Router>
-      <AppRouter />
-    </Router>
+    <AppRouter />
   );
 }
 
